b5c755eb7e5d275cb448fc5ec527a59ae5dc35a8
renangobero <142431151+renangobero@users.noreply.github.com>
2025-06-18 14:06:52 -0300

    correção de calculo

--- Exemplos.Domain/ClasseDeCalculo.cs
+++ Exemplos.Domain/ClasseDeCalculo.cs
@@ -12,7 +12,7 @@ namespace Exemplos.Domain
         public void Iniciar()
         {
 
-            var valorDivisao = 2;
+            var valorDivisao = 0;
             var valor = 10;
             var novoValor = valor * 100;
             var novoValorDividido = valorDivisao != 0 ? novoValor / valorDivisao : 0;
