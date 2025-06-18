88f59b5ea620b5c3b954fcea09fa67d958c16115
Renan Gobero <fn.tc@hotmail.com>
2025-06-17 23:23:46 -0300

    adiciona classe teste

--- Exemplos.Domain/Class1.cs
+++ Exemplos.Domain/Class1.cs
@@ -8,5 +8,22 @@ namespace Exemplos.Domain
 {
     public class Class1
     {
+
+        public void Iniciar()
+        {
+
+            var valorDividir = 0;
+            var valor = 10;
+            var novoValor = valor * 100;
+            var novoValorDivido = novoValor / valorDividir;
+
+            Console.WriteLine(valor.ToString("n2"));
+            Console.WriteLine(novoValor.ToString("n2"));
+            Console.WriteLine(novoValorDivido.ToString("n2"));
+
+            Console.WriteLine("Classe iniciad");
+            Console.WriteLine("Obrgado!!!");
+
+        }
     }
 }
